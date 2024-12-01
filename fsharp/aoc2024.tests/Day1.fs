5397fdbd37e62cededd3c4004d5ac9369967e6bf
Tom <tom.kemper@hu.nl>
2024-12-01 14:22:16 +0100

    Ok, de kop is er af

---
+++ fsharp/aoc2024.tests/Day1.fs
@@ -0,0 +1,28 @@
+﻿module AoC2024.Day1.Tests
+
+open System.IO
+open NUnit.Framework
+
+[<SetUp>]
+let Setup () = ()
+
+
+let example1 = """3   4
+4   3
+2   5
+1   3
+3   9
+3   3"""
+
+[<Test>]
+let day1Example () =
+    let exampleInput = example1.Split("\n")
+    Assert.AreEqual(11, day1 (List.ofArray exampleInput))
+    ()
+
+let input = File.ReadLines "./Day1.txt" |> List.ofSeq
+
+[<Test>]
+let day1A () =
+    Assert.AreEqual(0, day1 input)
+    ()
---
+++ fsharp/aoc2024/Day1.fs
@@ -0,0 +1,20 @@
+﻿module AoC2024.Day1
+
+let computeDistance (l1: int list, l2: int list) =
+    let (sls, srs) = (List.sort l1, List.sort l2)
+    List.zip sls srs |>
+        List.map (fun (a,b) -> a - b |> abs) |> List.sum
+    
+let day1 (input: string list) =
+    let splitLine (line: string) =
+        let parts = line.Split("   ")
+        (int parts.[0], int parts.[1])
+    let pairs = List.map splitLine input
+    
+    let toLists (ls, rs) (l,r) =
+        l:: ls, r :: rs
+    
+    List.fold toLists ([], []) pairs |>
+        computeDistance
+    
+    
