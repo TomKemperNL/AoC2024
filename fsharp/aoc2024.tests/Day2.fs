713862630fa993d5d6a3200806d1622f5b774aee
Tom <tom.kemper@hu.nl>
2024-12-02 07:12:34 +0100

    Dag 2B

--- fsharp/aoc2024.tests/Day2.fs
+++ fsharp/aoc2024.tests/Day2.fs
@@ -16,14 +16,25 @@ let example1 = """7 6 4 2 1
 1 3 6 7 9"""
 
 [<Test>]
-let day1Example () =
+let day2Example () =
     let exampleInput = example1.Split("\n")
     Assert.AreEqual(2, day2 (List.ofArray exampleInput))
+    ()
+
+[<Test>]
+let day2BExample () =
+    let exampleInput = example1.Split("\n")
+    Assert.AreEqual(4, day2B (List.ofArray exampleInput))
     ()
 
 let input = File.ReadLines "./Day2.txt" |> List.ofSeq
 
 [<Test>]
 let day2A () =
-    Assert.AreEqual(0, day2 input)
+    Assert.AreEqual(502, day2 input)
+    ()
+    
+[<Test>]
+let day2B () =
+    Assert.AreEqual(544, day2B input)
     ()    
--- fsharp/aoc2024/Day2.fs
+++ fsharp/aoc2024/Day2.fs
@@ -11,8 +11,26 @@ let isSafe (r: Report) =
     let difOk = List.forall (fun (a, b) -> abs(a - b) <= 3) pairs
     (allUp || allDOwn) && difOk
 
+
+let permutationsExcept (r: Report) =
+    seq {
+        for i in 0..(List.length r - 1) do
+            yield List.take i r @ List.skip (i + 1) r
+    }
+       
+    
+let isSafeB (r: Report) =
+    let perms = permutationsExcept r
+    Seq.exists isSafe perms
+    
 let day2 (input: string list) =
     input 
     |> List.map (String.split " " >> List.map int)
     |> List.filter isSafe
+    |> List.length
+    
+let day2B (input: string list) =
+     input 
+    |> List.map (String.split " " >> List.map int)
+    |> List.filter isSafeB
     |> List.length
