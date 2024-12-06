ae4e8b4ea8f1acd3718caeda7ea7696c5cdb4446
Tom <tom.kemper@hu.nl>
2024-12-06 22:02:09 +0100

    Het is een partial order, en daar kun je dingen mee... maar for-loop goes brr I suppose

---
+++ fsharp/aoc2024.tests/Day5.fs
@@ -0,0 +1,48 @@
+﻿module AoC2024.Day5.Tests
+
+open System.IO
+open NUnit.Framework
+
+
+let exampleInput = """47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47"""
+
+[<Test>]
+let day5Example () =
+    let exampleInput = exampleInput.Split("\r\n")
+    Assert.AreEqual(143, day5 (List.ofArray exampleInput))
+    ()
+    
+
+let input = File.ReadAllLines "./Day5.txt" |> Array.toList
+
+[<Test>]
+let day5A () =
+    Assert.AreEqual(4959, day5 input)
+    ()
---
+++ fsharp/aoc2024/Day5.fs
@@ -0,0 +1,27 @@
+﻿module AoC2024.Day5
+
+type OrderRule = int * int
+type Update = int list
+
+module OrderRule =
+    let satisfies rule update =
+        let minor, major = rule
+        match List.tryFindIndex ((=) minor) update with
+        | Some ix ->
+            match List.tryFindIndex ((=) major) update with
+            | Some ix2 -> ix < ix2
+            | None -> true
+        | None -> true
+
+
+
+let day5 (input: string list) =
+    let rules, updates = input 
+                         |> List.filter (fun (s: string) -> s.Length > 0)
+                         |> List.partition (fun s -> s.Contains "|")
+    let rules = rules |> List.map (fun s -> s.Split "|" |> Array.map int |> (fun [|a;b|] -> a, b))
+    let updates = updates |> List.map (fun s -> s.Split "," |> Array.map int |> Array.toList)
+    let validUpdates = updates
+                       |> List.filter (fun u -> rules |> List.forall (fun r -> OrderRule.satisfies r u))
+    let middles = List.map (fun u -> List.item (List.length u / 2) u) validUpdates
+    List.sum middles
