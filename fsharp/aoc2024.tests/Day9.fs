86e8f52220d55181615823b323466379c5c02f47
Tom <tom.kemper@hu.nl>
2024-12-09 07:24:01 +0100

    Boilerplate

---
+++ fsharp/aoc2024.tests/Day9.fs
@@ -0,0 +1,20 @@
+﻿module AoC2024.Day9.Tests
+
+open System.IO
+open NUnit.Framework
+
+
+let exampleInput = """2333133121414131402"""
+
+[<Test>]
+let day4Example () =    
+    Assert.AreEqual(1928, day9 exampleInput)
+    ()
+    
+
+let input = File.ReadAllText "./Day4.txt"
+
+[<Test>]
+let day4A () =
+    Assert.AreEqual(0, day9 input)
+    ()    
---
+++ fsharp/aoc2024/Day9.fs
@@ -0,0 +1,4 @@
+﻿module AoC2024.Day9
+
+let day9 input =
+    42
